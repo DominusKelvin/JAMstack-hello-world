c7723a5336f0b85eac5ea3aff1723985a41944b8
Kelvin Omereshone <kelvinomereshone@gmail.com>
2018-08-28 09:58:36 +0100

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi people</h1>
+    <p>I am Kelvin Omereshone an aspiring world class fullstack developer. Works as a front-end developer @ <a href="https:/mypadi.ng">myPadi.ng</a></p>
+    <p>Love JS! Find me on <a href="https://www.github.com/dominuskelvin">github @dominuskelvin</a></p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
